091a23458d1a85303980b0f1ee45241962d1d6e8
Marta <m.perezr.2021@alumnos.urjc.es>
2024-03-17 11:48:05 +0100

    update ej 2

--- consultas ej 2.py
+++ consultas ej 2.py
@@ -23,3 +23,31 @@ print("Media:")
 print(df_dates.groupby('user_id').count().mean()['fecha'])
 print("Desviacion estandar:")
 print(df_dates.groupby('user_id').count().std()['fecha'])
+
+#media y desviacion estandar del total de IPs que se han detectado
+print("Media y desviacion estandar del total de IPs que se han detectado")
+print("Media:")
+print(df_dates.groupby('user_id').count().mean()['ip'])
+print("Desviacion estandar:")
+print(df_dates.groupby('user_id').count().std()['ip'])
+
+#media y desviacion estandar del numero de emails recibidos de phising en los que ha interactuado cualquier usuario
+print("Media y desviación estándar del número de email recibidos de phishing en los que ha interactuado cualquier usuario")
+print("Media:")
+print(df_users['emails_phising'].mean())
+print(("Desviacion estandar:"))
+print(df_users['emails_phising'].std())
+
+#valor minimo y valor maximo del total de emails recibidos
+print("Valor minimo y valor maximo del total de emials recibidos")
+print("Minimo:")
+print(df_users['emails_total'].min())
+print("Maximo:")
+print(df_users['emails_total'].max())
+
+#valor minimo y valor maximo del numero de emails phising en los que ha interactuado un administrador
+print("Valor mínimo y valor máximo del número de emails phishing en los que ha interactuado un administrador")
+print("Minimo:")
+print(df_admin['emails_clicked'].min())
+print("Maximo:")
+print(df_admin['emails_clicked'].max())
